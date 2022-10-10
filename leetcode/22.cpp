f6716d4a4de3968dc19c4525ac9e2dc37a1af177
taeyangcode <coreymostero@gmail.com>
2022-10-10 00:02:06 -0700

    Leetcode
    Solution

--- leetcode/22.cpp
+++ leetcode/22.cpp
@@ -1,6 +1,8 @@
 #include <unordered_map>
 #include <vector>
 
+// Solution 1
+
 class Solution {
    private:
     std::unordered_map<std::string, std::string> map;
@@ -31,5 +33,35 @@ class Solution {
                 this->map[newStr] = newStr;
             }
         }
+    }
+};
+
+// Solution 2
+
+#include <vector>
+
+class Solution {
+   public:
+    std::vector<std::string> generateParenthesis(int n) {
+        std::vector<std::string> result;
+        this->generate(result, n, "(", 1, 0);
+        return result;
+    }
+
+    void generate(std::vector<std::string>& list, int& n, std::string current, int open, int closed) {
+        if (open + closed == n * 2) {
+            list.push_back(current);
+            return;
+        }
+        if (open == closed) {
+            this->generate(list, n, current + "(", open + 1, closed);
+            return;
+        }
+        if (open == n) {
+            this->generate(list, n, current + ")", open, closed + 1);
+            return;
+        }
+        this->generate(list, n, current + "(", open + 1, closed);
+        this->generate(list, n, current + ")", open, closed + 1);
     }
 };
