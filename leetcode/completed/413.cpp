72f1066018bec3b21a68fb7f1447cb7f1247056b
taeyangcode <coreymostero@gmail.com>
2023-01-30 17:18:57 -0800

    update to 413

--- leetcode/completed/413.cpp
+++ leetcode/completed/413.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/arithmetic-slices/
 
+// Initial Solution - Recursive
+
 #include <vector>
 
 class Solution {
@@ -11,22 +13,49 @@ class Solution {
             return 0;
         }
 
-        int result = 0;
-        for (std::size_t index = 0; index < size - 2; ++index) {
-            const int slices = this->count_slices(numbers, index + 1, numbers[index + 1] - numbers[index], size);
-            index += slices;
-            result += (slices * slices + slices) / 2;
+        int result = 0, streak = 0;
+        for (std::size_t index = 2, difference = numbers[1] - numbers[0]; index < size; ++index) {
+            const int current_difference = numbers[index] - numbers[index - 1];
+            if (current_difference == difference) {
+                ++streak;
+            }
+            else {
+                result += (streak * streak + streak) / 2;
+                difference = current_difference;
+                streak = 0;
+            }
         }
+        result += (streak * streak + streak) / 2;
         return result;
     }
+};
 
-    int count_slices(const std::vector<int>& numbers, const std::size_t index, const int change, const std::size_t size) {
-        if (index == size - 1) {
+// Iterative solution
+
+#include <vector>
+
+class Solution {
+   public:
+    int numberOfArithmeticSlices(std::vector<int>& numbers) {
+        const std::size_t& size = numbers.size();
+
+        if (size < 2) {
             return 0;
         }
-        if (numbers[index + 1] - numbers[index] == change) {
-            return 1 + count_slices(numbers, index + 1, change, size);
+
+        int result = 0, streak = 0;
+        for (std::size_t index = 2, difference = numbers[1] - numbers[0]; index < size; ++index) {
+            const int current_difference = numbers[index] - numbers[index - 1];
+            if (current_difference == difference) {
+                ++streak;
+            }
+            else {
+                result += (streak * streak + streak) / 2;
+                difference = current_difference;
+                streak = 0;
+            }
         }
-        return 0;
+        result += (streak * streak + streak) / 2;
+        return result;
     }
 };
