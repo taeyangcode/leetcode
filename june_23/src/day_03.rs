9d16407d26d1ce98d18dd9d8be586f70e72b6963
taeyangcode <coreymostero@gmail.com>
2023-06-27 01:25:31 -0700

    leetcode: june 03

--- june_23/src/day_03.rs
+++ june_23/src/day_03.rs
@@ -8,14 +8,16 @@ impl Solution {
     pub fn num_of_minutes(n: i32, head_id: i32, manager: Vec<i32>, inform_time: Vec<i32>) -> i32 {
         let mut result: i32 = 0;
         let mut cache: HashMap<i32, i32> = HashMap::new();
-        for index in 0..n {
-            let manager_index: &i32 = &manager[index as usize];
-            if cache.get(manager_index).is_none() {
-                cache.insert(*manager_index, 1);
-                result += inform_time[*manager_index as usize];
-            }
+        cache.insert(head_id, 0);
+
+        for index in 0..n as usize {
         }
 
         return result;
+    }
+
+    fn find_num_of_minutes(n: &usize, head_id: &usize, manager: &Vec<i32>, inform_time: &Vec<i32>, cache: &mut HashMap<i32, i32>, index: usize) -> () {
+        match cache.get(&manager[index]) {
+        };
     }
 }
