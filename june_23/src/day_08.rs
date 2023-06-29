fe84b8f3cc28398141a5c8599839439bd71672f6
taeyangcode <coreymostero@gmail.com>
2023-06-29 15:04:43 -0700

    daily: june 08

---
+++ june_23/src/day_08.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+pub struct Solution;
+
+impl Solution {
+    pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
+        return grid.into_iter().flatten().collect::<Vec<i32>>().into_iter().fold(0, |accumulator: i32, element: i32| accumulator + ((element < 0) as i32));
+    }
+}
--- june_23/src/lib.rs
+++ june_23/src/lib.rs
@@ -8,6 +8,7 @@ mod day_04;
 mod day_05;
 mod day_06;
 mod day_07;
+mod day_08;
 
 #[cfg(test)]
 mod test {
