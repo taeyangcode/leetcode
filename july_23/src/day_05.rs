bba96fbae59cfc98bcf86c1a2843ad9f243bb8fc
taeyangcode <coreymostero@gmail.com>
2023-07-05 16:45:54 -0700

    daily: july 05

---
+++ july_23/src/day_05.rs
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+// https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/submissions/987340024/
+
+pub struct Solution;
+
+impl Solution {
+    pub fn longest_subarray(nums: Vec<i32>) -> i32 {
+        let length: i32 = nums.len() as i32;
+        let (mut max, mut zero_index, mut streak): (i32, i32, i32) = (0, -1, 0);
+        for current_index in 0..length {
+            match nums[current_index as usize] {
+                0 => {
+                    match zero_index {
+                        -1 => zero_index = current_index,
+                        index => {
+                            streak = current_index - zero_index - 1;
+                            zero_index = current_index;
+                        },
+                    };
+                },
+                1 => streak += 1,
+                _ => {},
+            };
+            if streak > max {
+                max = streak;
+            }
+        }
+        if zero_index == length - 1 && max == 0 {
+            return 0;
+        }
+        if zero_index == -1 {
+            return length as i32 - 1;
+        }
+        return max;
+    }
+}
