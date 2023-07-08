07708b73e3cbb451f99191488bbacb8911ddbe60
taeyangcode <coreymostero@gmail.com>
2023-07-08 03:30:24 -0700

    daily: july 05

--- july_23/src/day_06.rs
+++ july_23/src/day_06.rs
@@ -2,41 +2,52 @@
 
 pub struct Solution;
 
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
         let mut length: usize = nums.len();
-        let mut subarray: Vec<i32> = vec![];
+        let mut subarray: VecDeque<i32> = VecDeque::new();
         let mut sum: i32 = 0;
 
-        return Self::find_subarray(target, &nums, &length, &mut subarray, &mut sum, 0);
+        Self::find_subarray(target, &nums, &length, &mut subarray, &mut sum, 0);
+
+        return subarray.len() as i32;
     }
 
-    fn find_subarray(target: i32, nums: &Vec<i32>, length: &usize, subarray: &mut Vec<i32>, sum: &mut i32, index: usize) -> i32 {
-        if index >= length {
-            return 0;
+    fn find_subarray(target: i32, nums: &Vec<i32>, length: &usize, subarray: &mut VecDeque<i32>, sum: &mut i32, index: usize) {
+        if index >= *length {
+            return;
         }
 
         *sum += nums[index];
-        subarray.push(nums[index]);
+        subarray.push_back(nums[index]);
 
         if *sum <= target {
-            return 1 + Self::find_subarray(target, nums, length, subarray, sum, index);
+            Self::find_subarray(target, nums, length, subarray, sum, index + 1);
+            return;
         }
 
-        while *sum - subarray[0] >= target {
-            *sum -= subarray[0];
-            subarray.remove(0);
-        }
-        let mut left_sum: i32 = subarray[0];
-        let mut right_sum: i32 = nums[index];
-        let mut current_index: usize = 1;
-        while left_sum < right_sum && index + current_index < length {
-            left_sum += subarray[current_index];
-            right_sum += nums[index + current_index];
-            current_index += 1;
-        }
-        if left_sum < right_sum {
-            return;
+        let mut last_index: usize = index;
+        loop {
+            println!("sum: {sum}, subarray: {:?}, index: {index}", subarray);
+            match std::cmp::min(subarray[0], nums[last_index]) {
+                first if first == subarray[0] => {
+                    println!("B");
+                    if *sum - first < target {
+                        println!("A");
+                        Self::find_subarray(target, nums, length, subarray, sum, index + 1);
+                        return;
+                    }
+                    *sum -= first;
+                    subarray.pop_front();
+                },
+                last => {
+                    if *sum - last < target {
+                        return;
+                    }
+                },
+            };
         }
     }
 }
