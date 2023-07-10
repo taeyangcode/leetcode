c9fee9aa6233a42321945b49c024d388f139182f
taeyangcode <coreymostero@gmail.com>
2023-07-09 17:15:14 -0700

    daily: july 06

--- july_23/src/day_06.rs
+++ july_23/src/day_06.rs
@@ -1,33 +1,27 @@
 // https://leetcode.com/problems/minimum-size-subarray-sum/
+// https://leetcode.com/problems/minimum-size-subarray-sum/submissions/990536561/
 
 pub struct Solution;
 
 impl Solution {
     pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
-        let mut subarray: Vec<i32> = vec![];
-        let mut nums_index: usize = 0;
-        let mut current_sum: i32 = std::i32::MAX;
+        let mut left_index: usize = 0;
+        let mut right_index: usize = 0;
+        let mut sum: i32 = 0;
+        let mut minimum_length: usize = std::usize::MAX;
 
-        Self::find_minimum_subarray(&target, &nums, &nums.len(), &mut subarray, nums_index, &mut current_sum);
-
-        return subarray.len() as i32;
-    }
-
-    fn find_minimum_subarray(target: &i32, nums: &Vec<i32>, length: &usize, subarray: &mut Vec<i32>, nums_index: usize, current_sum: &mut i32) {
-        if *current_sum + nums[nums_index] <= *target {
-            *current_sum += nums[nums_index];
-            subarray.push(nums[nums_index]);
-            Self::find_minimum_subarray(target, nums, length, subarray, nums_index + 1, current_sum);
-            return;
+        for number in &nums {
+            sum += number;
+            if sum >= target {
+                while sum >= target {
+                    minimum_length = std::cmp::min(minimum_length, right_index - left_index + 1);
+                    sum -= nums[left_index];
+                    left_index += 1;
+                }
+            }
+            right_index += 1;
         }
 
-        let new_sum: i32 = *current_sum + nums[nums_index];
-        if new_sum - subarray[0] >= *target {
-            *current_sum += nums[nums_index] - subarray[0];
-            subarray.remove(0);
-            subarray.push(nums[nums_index]);
-            Self::find_minimum_subarray(target, nums, length, subarray, nums_index, current_sum);
-            return;
-        }
+        return (minimum_length != std::usize::MAX) as i32 * minimum_length as i32;
     }
 }
