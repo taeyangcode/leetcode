396e6a65c3f6a9efadee1a0fc0830beb12a63333
taeyangcode <coreymostero@gmail.com>
2023-07-23 01:28:07 -0700

    daily: july 08

--- july_23/src/day_08.rs
+++ july_23/src/day_08.rs
@@ -4,6 +4,17 @@ pub struct Solution;
 
 impl Solution {
     pub fn put_marbles(weights: Vec<i32>, k: i32) -> i64 {
+        let weights_length: usize = weights.len();
+        let mut bags: Vec<Vec<i32>> = vec![vec![]; k as usize];
+
+        let mut bag_index: usize = 0;
+        let mut weight_index: usize = 0;
+        while weight_index < weights_length {
+            bags[bag_index].push(weights[weight_index]);
+            weight_index += 1;
+            bag_index += (weight_index > weights_length - k as usize) as usize;
+        }
+
         return -1;
     }
 }
--- july_23/src/lib.rs
+++ july_23/src/lib.rs
@@ -9,3 +9,13 @@ mod day_06;
 mod day_07;
 mod day_08;
 mod day_11;
+
+#[cfg(test)]
+mod test_day_08 {
+    use crate::day_08::Solution;
+
+    #[test]
+    fn test_01() {
+        Solution::put_marbles(vec![1, 2, 3, 4, 5, 6, 7], 3);
+    }
+}
