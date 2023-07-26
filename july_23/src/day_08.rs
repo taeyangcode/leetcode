1cf4164ea7a55e5f869477e1c51dbc671afa1d36
taeyangcode <coreymostero@gmail.com>
2023-07-25 20:01:27 -0700

    daily: july 08

--- july_23/src/day_08.rs
+++ july_23/src/day_08.rs
@@ -2,19 +2,55 @@
 
 pub struct Solution;
 
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn put_marbles(weights: Vec<i32>, k: i32) -> i64 {
         let weights_length: usize = weights.len();
-        let mut bags: Vec<Vec<i32>> = vec![vec![]; k as usize];
+        let mut bags: Vec<VecDeque<i32>> = vec![VecDeque::new(); k as usize];
 
         let mut bag_index: usize = 0;
         let mut weight_index: usize = 0;
         while weight_index < weights_length {
-            bags[bag_index].push(weights[weight_index]);
+            bags[bag_index].push_back(weights[weight_index]);
             weight_index += 1;
             bag_index += (weight_index > weights_length - k as usize) as usize;
         }
 
-        return -1;
+        let mut min: i64 = std::i64::MAX;
+        let mut max: i64 = std::i64::MIN;
+        Self::find_bags(&mut bags, &mut max, &mut min, &(k as usize), 0);
+
+        return max - min;
+    }
+
+    fn find_bags(bags: &mut Vec<VecDeque<i32>>, max: &mut i64, min: &mut i64, bag_limit: &usize, bag_index: usize) -> () {
+        let current_score: i64 = bags
+            .iter()
+            .fold(0, |accumulator: i64, current_bag: &VecDeque<i32>|
+                  accumulator + *current_bag.front().unwrap() as i64 + *current_bag.back().unwrap() as i64);
+        *max = std::cmp::max(*max, current_score);
+        *min = std::cmp::min(*min, current_score);
+        println!("{:?}", bags);
+        println!("current score: {current_score}, max: {max}, min: {min}");
+
+        let has_one_marble: bool = bags[bag_index].len() == 1;
+        let is_last_bag: bool = *bag_limit == bag_index + 1;
+        if has_one_marble || is_last_bag {
+            return;
+        }
+
+        loop {
+            if bags[bag_index].len() == 1 {
+                let next_element: i32 = bags[bag_index + 1].pop_front().unwrap();
+                bags[bag_index].push_back(next_element);
+                return;
+            }
+
+            let pop_element: i32 = bags[bag_index].pop_back().unwrap();
+            bags[bag_index + 1].push_front(pop_element);
+
+            Self::find_bags(bags, max, min, bag_limit, bag_index + 1);
+        }
     }
 }
--- july_23/src/day_11.rs
+++ july_23/src/day_11.rs
@@ -2,6 +2,7 @@
 
 pub struct Solution;
 
+/*
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
@@ -20,36 +21,4 @@ impl TreeNode {
         }
     }
 }
-
-use std::rc::{Rc, Weak};
-use std::cell::RefCell;
-use std::collections::VecDeque;
-
-impl Solution {
-    pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut queue: VecDeque<Weak<RefCell<TreeNode>>> = VecDeque::new();
-
-        fn find_min_depth(node: Rc<RefCell<TreeNode>>, queue: &mut VecDeque<Weak<RefCell<TreeNode>>>) -> i32 {
-            let borrow_node = node.borrow();
-            let left_node = borrow_node.left.as_ref().map(Rc::downgrade);
-            let right_node = borrow_node.right.as_ref().map(Rc::downgrade);
-
-            match (left_node, right_node) {
-                (Some(left_node), Some(right_node)) => {
-                    queue.push_back(left_node);
-                    queue.push_back(right_node);
-                },
-                (Some(left_node), None) => queue.push_back(left_node),
-                (None, Some(right_node)) => queue.push_back(right_node),
-                (None, None) => return 1,
-            };
-
-            1 + find_min_depth(queue.pop_front().unwrap().upgrade().unwrap(), queue)
-        }
-
-        match root {
-            Some(node) => find_min_depth(node, &mut queue),
-            None => 0,
-        }
-    }
-}
+*/
