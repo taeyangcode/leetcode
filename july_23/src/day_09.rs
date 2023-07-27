ab570f7cffab0f808b875dc98530f4c6959c92eb
taeyangcode <coreymostero@gmail.com>
2023-07-27 16:47:22 -0700

    daily: july 09

---
+++ july_23/src/day_09.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/substring-with-largest-variance/
+
+pub struct Solution;
+
+impl Solution {
+    pub fn largest_variance(s: String) -> i32 {
+        return -1;
+    }
+}
