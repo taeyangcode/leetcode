dfa0fe4e06082c4e4af0e0645ed8610880be4810
taeyangcode <coreymostero@gmail.com>
2023-08-07 11:02:34 -0700

    daily: august 7

---
+++ august_23/src/day_07.rs
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/search-a-2d-matrix/
+
+struct Solution;
+
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+    }
+}
--- august_23/src/lib.rs
+++ august_23/src/lib.rs
@@ -3,3 +3,5 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
+mod day_07;
