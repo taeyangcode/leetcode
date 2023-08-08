7c10d88aa9bc908e12a42c852f05ea3df6ecd846
taeyangcode <coreymostero@gmail.com>
2023-08-08 10:59:16 -0700

    file tree: removed leetcode vscode config directory how did this get here

---
+++ august_23/src/day_08.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+struct Solution;
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let length: usize = nums.len();
+        let mut current_index: usize = length / 2;
+    }
+}
